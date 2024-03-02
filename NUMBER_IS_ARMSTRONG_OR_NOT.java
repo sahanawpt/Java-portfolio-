42cb39b3d691fdf4ed8f44415ddfb5be5da1ca02
sahanawpt <161597738+sahanawpt@users.noreply.github.com>
2024-03-02 22:06:58 +0530

    WEEK_5 PROGRAMM_6  Learn and understand do while loop construct, demonstrate the same to check whether given number is Armstrong number or not?

---
+++ NUMBER_IS_ARMSTRONG_OR_NOT.java
@@ -0,0 +1,25 @@
+package Week5;
+import java.util.*;
+public class NUMBER_IS_ARMSTRONG_OR_NOT {
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the value of n:");
+		int n=sc.nextInt();
+		int sum=0;
+		int num=n;
+		int rem=0;
+		do 
+		{
+			rem=num%10;
+			sum=sum+(rem*rem*rem);
+			num=num/10;
+		}
+		while(num>0);
+		if(sum==n){
+		System.out.println("number is armstrong number");
+		}
+		else{
+		System.out.println("number is not a armstrong number");
+		}
+	}
+}

756db755d16d1f2e7a515b62578d340f1b80df98
sahanawpt <161597738+sahanawpt@users.noreply.github.com>
2024-03-14 23:14:53 +0530

    Week_1 Check positive or negative number

---
+++ newfile.java
@@ -0,0 +1,20 @@
+package week;
+public class PositiveOrNegetive {
+	public static void main(String args[])
+	{
+		int num=912;
+		if(num>0)
+		{
+			System.out.println("the given number is positive");
+		}
+		else if(num<0)
+		{
+			System.out.println("the given number is negetive");
+		}
+		else
+		{
+			System.out.println("the given number is zero");
+		}
+	}
+	
+}
